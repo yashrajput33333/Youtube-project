6083bb0d186013e5fe34b79c3844d61b7003112d
yashrajput33333 <yashrajput33333@gmail.com>
2025-01-18 14:31:31 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ import {DB_NAME} from "./constants.js"
 import connectDB from "./db/index.js"
 
 connectDB() 
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed ", err)
+})
+
 
 
 
